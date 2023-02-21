6b28e4e69704852e17221c23652b7ec5235e5624
Fabiola <105880646+FabiDeG@users.noreply.github.com>
2023-02-21 03:03:12 -0600

    Creación interfaz que contiene cada operación

--- src/Main/ICalcu.java
+++ src/Main/ICalcu.java
@@ -1,5 +1,15 @@
 package Main;
 
+// Creamos cada una de las operaciones que se logran realizar
+
 public interface ICalcu {
+	
+	public float Suma(float num1, float num2);
+	
+	public float Resta(float num1, float num2);
+	
+	public float Dividir(float num1, float num2);
+	
+	public float Multiplicar(float num1, float num2);
 
 }
