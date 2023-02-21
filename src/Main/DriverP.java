d7e494c12237003654b403b845985505fac48f19
Fabiola <105880646+FabiDeG@users.noreply.github.com>
2023-02-21 03:45:25 -0600

    Update DriverP.java

--- src/Main/DriverP.java
+++ src/Main/DriverP.java
@@ -7,8 +7,14 @@ import java.io.FileNotFoundException;
 public class DriverP {
 
 	public static void main(String[] args) throws FileNotFoundException{
-		// TODO Auto-generated method stub
 
+		String archivo = "datos.txt";
+		File arch = new File(archivo);
+		Scanner scanner = new Scanner(arch);
+		String inflix = "";
+		inflix = scanner.nextLine();
+		String[] items = inflix.split(" ");
+		
 	}
 
 }
