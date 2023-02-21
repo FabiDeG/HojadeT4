58d2ce968b9b7f8b77d3cdde97f1e687c938636d
Fabiola <105880646+FabiDeG@users.noreply.github.com>
2023-02-21 03:39:24 -0600

    Update DriverP.java

--- src/Main/DriverP.java
+++ src/Main/DriverP.java
@@ -1,11 +1,12 @@
 package Main;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 
 public class DriverP {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException{
 		// TODO Auto-generated method stub
 
 	}
